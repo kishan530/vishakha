a7b6ea9c8020736c4ae3afa3592566369ae61824
baddala <baddala156@gmail.com>
2017-07-20 18:50:45 +0530

    products page
    products page

--- header.php
+++ header.php
@@ -94,9 +94,11 @@
                             <ul class="navigation clearfix">
                                 <li class="current" ><a href="index.php">Home</a></li>
                                 <li><a href="about-us.php">About</a></li>
+								<li><a href="products.php">Products</a></li>
                                 <li><a href="services.php">Services</a></li>                               
                                 <li><a href="portfolio.php">Portfolio</a></li>                                
                                 <li><a href="contact.php">Contact</a></li>
+								
                             </ul>
                         </div>
                     </nav>
@@ -132,9 +134,11 @@
                             <ul class="navigation clearfix">
                                  <li class="current" ><a href="index.php">Home</a></li>
                                 <li><a href="about-us.php">About</a></li>
+								<li><a href="products.php">Products</a></li>
                                 <li><a href="services.php">Services</a></li>                               
                                 <li><a href="portfolio.php">Portfolio</a></li>                                
                                 <li><a href="contact.php">Contact</a></li>
+								
                             </ul>
                         </div>
                     </nav><!-- Main Menu End-->
